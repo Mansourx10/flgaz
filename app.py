7e7f50485c6d2f6897fd2fa77f132788834d7e87
MansourX10 <mahamat.mansour@yahoo.com>
2019-11-29 13:49:30 +0000

    Restriction de message long + add new route

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ def home():
 def save_gazouille():
 	if request.method == 'POST':
 		print(request.form)
-		dump_to_csv(request.form)
-		return redirect(url_for('timeline'))
-		#return "OK"
+		if len(request.form["user-name"]) < 20 and len(request.form["user-text"]) <= 280:
+		    dump_to_csv(request.form)
+		    return redirect(url_for('timeline'))
+		  #return "OK"
+		else:
+		    return "message trop long"
+
 	if request.method == 'GET':
 		return render_template('formulaire.html')
 
@@ -27,7 +31,8 @@ def parse_from_csv():
 	with open('./gazouilles.csv', 'r') as f:
 		reader = csv.reader(f)
 		for row in reader:
-			gaz.append({"user":row[0], "text":row[1]})
+		    if len(row[1]) <= 280:
+		        gaz.append({"user":row[0], "text":row[1]})
 	return gaz
 
 def dump_to_csv(d):
@@ -35,3 +40,18 @@ def dump_to_csv(d):
 	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
 		writer = csv.writer(f)
 		writer.writerow(donnees)
+
+@app.route('/timeline/<name>/', methods=['GET'])
+def get_timeline(name):
+    print(name)
+    gaz = get_timeline_user(name)
+    return render_template("timeline.html", gaz = gaz)
+
+def get_timeline_user(name):
+    gaz = []
+    with open('./gazouilles.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == name:
+                gaz.append({"user":row[0], "text":row[1]})
+    return gaz
